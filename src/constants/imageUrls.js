907602fb3daa6fbfd9d5c2b9032571cb1187a07e
Yen Jung Chen <jenny610138@gmail.com>
2021-12-27 12:39:14 +0800

    chore/update-ab-image-urls

--- src/constants/imageUrls.js
+++ src/constants/imageUrls.js
@@ -1,7 +1,11 @@
 export const imageUrls = [
-  "https://storage.googleapis.com/baby-dragon/public/AccountingBookIcon/AccoutingBookIcon-v1-0.jpg",
-  "https://storage.googleapis.com/baby-dragon/public/AccountingBookIcon/AccoutingBookIcon-v1-1.jpg",
-  "https://storage.googleapis.com/baby-dragon/public/AccountingBookIcon/AccoutingBookIcon-v1-2.jpg",
+  "https://storage.googleapis.com/baby-dragon/public/AccountingBookIcon/AccoutingBookIcon-v2-1.png",
+  "https://storage.googleapis.com/baby-dragon/public/AccountingBookIcon/AccoutingBookIcon-v2-2.png",
+  "https://storage.googleapis.com/baby-dragon/public/AccountingBookIcon/AccoutingBookIcon-v2-3.png",
+  "https://storage.googleapis.com/baby-dragon/public/AccountingBookIcon/AccoutingBookIcon-v2-4.png",
+  "https://storage.googleapis.com/baby-dragon/public/AccountingBookIcon/AccoutingBookIcon-v2-5.png",
+  "https://storage.googleapis.com/baby-dragon/public/AccountingBookIcon/AccoutingBookIcon-v2-6.png",
+  "https://storage.googleapis.com/baby-dragon/public/AccountingBookIcon/AccoutingBookIcon-v2-7.png",
 ]
 export const userImageUrls = [
   "https://storage.googleapis.com/baby-dragon/public/AccountingBookIcon/AccoutingBookIcon-v1-0.jpg",
