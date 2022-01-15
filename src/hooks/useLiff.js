d2c9fb8d972673dba165af3b996343b07a778a17
Yen Jung Chen <jenny610138@gmail.com>
2022-01-15 16:15:33 +0800

    update/babydragon-net

--- src/hooks/useLiff.js
+++ src/hooks/useLiff.js
@@ -23,7 +23,7 @@ const useLiff =  (callback) => {
       .then(() => {
         setIsLoggedIn(true)
         if (!liff.getAccessToken()) {
-          liff.login({ redirectUri: `https://baby-dragon-web.herokuapp.com${window.location.pathname}` })
+          liff.login({ redirectUri: `https://www.dragonbaby.net${window.location.pathname}` })
         } else {
           api.exchangeToken(liff.getAccessToken(), liff.getDecodedIDToken())
             .then((res) => {
