a7a47c7a2a21dc3a07ac50d582a0789a560e23f9
Yen Jung Chen <jenny610138@gmail.com>
2022-01-22 12:26:35 +0800

    add/payment-create-average

--- src/components/FormElements/Section/Section.js
+++ src/components/FormElements/Section/Section.js
@@ -3,11 +3,15 @@ import styles from './Section.module.scss';
 
 const section = ({
   name,
+  children,
   style
 }) => {
   return (
     <div style={style ? style: {}} className={styles.container}>
       {name}
+      <div className={styles.lint}>
+        {children}
+      </div>
     </div>
   );
 };
